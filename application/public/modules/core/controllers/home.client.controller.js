c7408c059d5813d2cb82625fed251dd332029827
JohnBarton27 <johnrbarton@ufl.edu>
2015-06-23 18:42:42 -0500

    Fixes #80 (Remove "Sign-In"/"Sign-Up" from Splash page if logged in)

--- application/public/modules/core/controllers/home.client.controller.js
+++ application/public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,13 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-	}
+        
+        $scope.showSignUp = function() {
+            if ($scope.authentication.user != "") {
+                return false;
+            } else {
+                return true;
+            }
+        }
+	}                                 
 ]);

f7ad97f81217e4201993a2ccb5f5f33888ffef33
JohnBarton27 <johnrbarton@ufl.edu>
2015-07-12 20:51:08 -0400

    Closes #150 (Write Protractor test for Creating Users)

--- application/tests/create-users.js
+++ application/tests/create-users.js
@@ -1,53 +1,117 @@
-describe('angularjs sign-up', function() {
+describe('angularjs create users', function() {
   
+	var newUserButton = element(by.id('new-user-button'));
+	
     var firstName = element(by.model('credentials.firstName'));
     var lastName = element(by.model('credentials.lastName'));
     var email = element(by.model('credentials.username'));
     var password = element(by.model('credentials.password'));
-    var signUpButton = element(by.id('sign-up-button'));    
+    var createButton = element(by.id('create-user-button'));    
     
     beforeEach(function() {
-        browser.get('http://localhost:3333/#!/signup');
-    });
-    
-    it('should have a title', function() {
-        expect(browser.getTitle()).toEqual('Azzenda');
-    });
-    
-    it('should allow sign-up', function() {
-        firstName.sendKeys('Walter');
-        lastName.sendKeys('White');
-        email.sendKeys('heisenberg@graymatter.com');
-        //username.sendKeys('Heisenberg');
-        password.sendKeys('password');
-        
-        signUpButton.click();
-        browser.waitForAngular();
         browser.get('http://localhost:3333/#!/users');
         browser.waitForAngular();
-        expect(element.all(by.repeater('user in users')).count()).toEqual(1);
+        newUserButton.click();
+        browser.waitForAngular();
+    }); 
+    
+    it('should allow creation (Skyler)', function() {        
+        firstName.sendKeys('Skyler');
+        lastName.sendKeys('White');
+        email.sendKeys('swhite@beneke.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(2);
     });
     
+    
+    it('should allow sign-up (Jesse)', function() {        
+        firstName.sendKeys('Jesse');
+        lastName.sendKeys('Pinkman');
+        email.sendKeys('capncook@heisenberg.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(3);
+    });
+    
+    it('should allow sign-up (Hank)', function() {        
+        firstName.sendKeys('Hank');
+        lastName.sendKeys('Schrader');
+        email.sendKeys('asacschrader@abqdea.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(4);
+    });
+    
+    it('should allow sign-up (Marie)', function() {        
+        firstName.sendKeys('Marie');
+        lastName.sendKeys('Schrader');
+        email.sendKeys('purple@ilovepurple.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(5);
+    });
+    
+    it('should allow sign-up (Walter White Jr.)', function() {        
+        firstName.sendKeys('Walter');
+        lastName.sendKeys('White');
+        email.sendKeys('flynn@savewalterwhite.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(6);
+    });
+    
+    it('should allow sign-up (Saul)', function() {        
+        firstName.sendKeys('Saul');
+        lastName.sendKeys('Goodman');
+        email.sendKeys('jmcgill@hhm.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(7);
+    });
+    
+    it('should allow sign-up (Mike)', function() {        
+        firstName.sendKeys('Mike');
+        lastName.sendKeys('Ehrmantraut');
+        email.sendKeys('ilovekaylee@imaboss.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(8);
+    });
+    
+    it('should allow sign-up (Lydia)', function() {        
+        firstName.sendKeys('Lydia');
+        lastName.sendKeys('Rodarte-Quayle');
+        email.sendKeys('lrodar@madrigal.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(9);
+    });
+    
+    it('should allow sign-up (Todd)', function() {        
+        firstName.sendKeys('Todd');
+        lastName.sendKeys('Alquist');
+        email.sendKeys('todd@vamonospest.com');
+        password.sendKeys('password');
+        
+        createButton.click();
+        browser.waitForAngular();
+        expect(element.all(by.repeater('user in users')).count()).toEqual(10);
+    });
 });
-
-
-
-/*
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    browser.get('https://angularjs.org');
-
-    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    element(by.css('[value="add"]')).click();
-
-    var todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(3);
-    expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-    // You wrote your first test, cross it off the list
-    todoList.get(2).element(by.css('input')).click();
-    var completedAmount = element.all(by.css('.done-true'));
-    expect(completedAmount.count()).toEqual(2);
-  });
-});
-*/
--- application/tests/pro-sign-up.js
+++ application/tests/pro-sign-up.js
@@ -1,4 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['create-users.js']
+  specs: ['sign-up.js']
 };

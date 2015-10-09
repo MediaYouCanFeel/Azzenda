728f7af04b178430eadfe583ced05f4731b76eb2
Benjamin Heuser <benjamin.heuser@gmail.com>
2015-10-08 22:00:11 -0400

    Got weekly recurring events working

--- application/app/controllers/eventRecurring/Weekly.js
+++ application/app/controllers/eventRecurring/Weekly.js
@@ -3,12 +3,33 @@
 /**
  * Module dependencies.
  */
+var moment = require('moment');
 
 /**
  * Get the next instance of the recurring this after curDate
  */
 exports.next = function(curDate) {
-	return this;
+	//console.log(curDate);
+	var weekDays = this.recurring.params.days;
+	var monthDate = moment(this.sched.start.getTime());
+	var curDate = moment(curDate);
+	var nextDate = moment(curDate).set({'hour': monthDate.hour(), 'minute': monthDate.minute(), 'second': 0});
+	while(weekDays.indexOf(nextDate.day()) == -1 || nextDate.isBefore(curDate)) {
+		nextDate.add(1, 'day');
+	}
+	var nDate = parseInt(nextDate.format('x'));
+	return {
+		_id: this._id,
+		name: this.name,
+		created: this.created,
+		owner: this.owner,
+		length: this.length,
+		sched: {
+			start: new Date(nDate),
+			end: new Date(nDate + this.length)
+		},
+		status: this.status
+	}
 };
 
 exports.past = function(curDate) {

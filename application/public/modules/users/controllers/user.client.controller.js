eb05ec7a1c37c64485c43ac516c954d8a01a617f
JohnBarton27 <johnrbarton@ufl.edu>
2015-11-25 23:33:07 -0500

    Profile pics now update correctly and the page reloads

--- application/public/modules/users/controllers/user.client.controller.js
+++ application/public/modules/users/controllers/user.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Rosters controller
-angular.module('users').controller('UsersController', ['$scope', '$stateParams', '$location', 'Authentication', 'Users',
-	function($scope, $stateParams, $location, Authentication, Users) {
+angular.module('users').controller('UsersController', ['$scope', '$stateParams', '$location', 'Authentication', 'Users', 'Upload', '$modal', '$state',
+	function($scope, $stateParams, $location, Authentication, Users, Upload, $modal, $state) {
 		$scope.authentication = Authentication;
 
 		// Remove existing User
@@ -22,28 +22,80 @@ angular.module('users').controller('UsersController', ['$scope', '$stateParams',
 			}
 		};
 
+		$scope.updateProfPicModal = function (size) {
+            
+            var modalInstance = $modal.open({
+              animation: $scope.animationsEnabled,
+              templateUrl: 'modules/users/views/update-prof-pic.client.view.html',
+              controller: function ($scope, $modalInstance, items) {
+            	  $scope.ok = function () {
+                      //$scope.selected.event
+                	  modalInstance.close();
+                  };
+
+                  $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                  };
+              },
+              size: size,
+              resolve: {
+                items: function () {
+                	//return $scope.events;
+                }
+              }
+            });
+            
+            modalInstance.result.then(function() {
+            	$scope.showProfPicUrl();
+            });
+        }; 
+        
+        
+        $scope.updateProfPic = function(croppedDataUrl) {
+        	$scope.setUrl(croppedDataUrl); 
+        	$scope.ok(); 
+        	$scope.showProfPicUrl();
+        	$scope.update();
+        	
+        }
+		
+        $scope.picSelected = function(files) {
+			if(files && (files.length > 0)) {
+				$stateParams.activePic = true;
+			} else {
+				$stateParams.activePic = false;
+			}
+		}
+        
+        $scope.setUrl = function(croppedDataUrl) {
+        	$stateParams.url = croppedDataUrl;
+        }
+        
+        $scope.showProfPicUrl = function() {
+        	$scope.url = $stateParams.url;
+        }
+        
 		// Update existing User
 		$scope.update = function() {
+			var userWantsToUpdateProfPic = true;
 			//this allows for the update of any member, not just the current user
 			var user = $scope.user;
 			if(userWantsToUpdateProfPic) {
 				Upload.upload({
-					url: '/users', 
+					url: '/users/' + $stateParams.userId + "/", 
 					method: 'PUT', 
 					headers: {'Content-Type': 'multipart/form-data'},
 					fields: {
-						activeImg: $stateParams.activePic,
+						activeImg: $stateParams.activePic
 						//after active img, list any fields you would like updated
 						//in the user object (other than prof pic). Only the fields
 						//you list will be updated. Only non-security related fields
 						// can be updated in this way (i.e. not password, roles, etc).
 						//Below are some examples. These are not required!
-						firstName: $scope.something1,
-						lastName: $scope.something2
 					},
 					file: Upload.dataUrltoBlob($scope.url)               
 				}).success(function (response, status) {
-					// Do something if it worked
+					$state.go($state.current, {}, {reload: true});
 				}).error(function (err) {
 					$scope.error = err.message;
 				});
